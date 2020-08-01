502dfd89a63f12adc70dd96ec35daeb74ebea65b
Waleed A. Afifi <waleedafifich@gmail.com>
2020-08-01 03:49:36 +0300

    Create hadelWeather function and getting data from API

--- server.js
+++ server.js
@@ -38,12 +38,15 @@ app.all('*', (req, res, next) => {
 // .env variables
 // ********
 let GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+let WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+let NUMBER_OF_DAY = process.env.NUMBER_OF_DAY;
 
 
 // ********
 // Routes
 // ********
 app.get('/location', handelLocation);
+app.get('/weather', handelWeather);
 
 
 // localhost:3000/location
@@ -55,29 +58,6 @@ app.get('/location', handelLocation);
 //     "longitude": "-122.332071"
 // }
 
-function handelLocation(req, res) {
-  let city = req.query.city;
-  let reqex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
-
-  if (!reqex.test(city)) { res.status(422).send({ 'status': 422, msg: 'Please enter a valid city name!'}); }
-  if(!city) { res.status(500).send({ 'status': 500, responseText: 'Sorry, something went wrong'}); }
-
-  getLocationData(city).then( returnedData => {
-    res.send(returnedData);
-  }).catch((err) => {
-    console.log(err.message);
-  });
-}
-
-function getLocationData(city) {
-  let url = `https://api.locationiq.com/v1/autocomplete.php?key=${GEOCODE_API_KEY}&q=${city}`;
-  let data = superagent.get(url).then((res) => {
-      console.log(res.body[0]);
-    return new Location(city, res.body[0]);
-  });
-  return data;
-
-}
 
 // localhost:3010/weather
 // Getting data from weather.json formated as
@@ -102,13 +82,50 @@ app.all('*', (req, res) => {
 // ********
 // Functions
 // ********
-function getWeather(req, res) {
-  Weather.all = [];
+
+function handelLocation(req, res) {
   let city = req.query.city;
-  let weatherData = require('./data/weather.json');
-  weatherData.data.map((e) => {
-    new Weather(city, e);
+  let reqex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+
+  if (!reqex.test(city)) { res.status(422).send({ 'status': 422, msg: 'Please enter a valid city name!'}); }
+  if(!city) { res.status(500).send({ 'status': 500, responseText: 'Sorry, something went wrong'}); }
+
+  getLocationData(city).then( returnedData => {
+    res.send(returnedData);
+  }).catch((err) => {
+    console.log(err.message);
   });
+}
+
+function getLocationData(city) {
+  let url = `https://api.locationiq.com/v1/autocomplete.php?key=${GEOCODE_API_KEY}&q=${city}`;
+  let data = superagent.get(url).then((res) => {
+    console.log(res.body[0]);
+    return new Location(city, res.body[0]);
+  });
+  return data;
+
+}
+
+
+function handelWeather(req, res) {
+  getWeather(req).then( returnedData => {
+    console.log('response: ', returnedData);
+    res.send(returnedData);
+  }).catch((err) => {
+    console.log(err.message);
+  });
+}
+
+function getWeather(req) {
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${req.query.city}&key=${WEATHER_API_KEY}&days=${NUMBER_OF_DAY}`;
+  let data = superagent.get(url).then((res) => {
+      console.log(res.body.data);
+    return res.body.data.map((e) => {
+      return new Weather(e);
+    });
+  });
+  return data;
 }
 
 // Location Constructor
@@ -121,7 +138,7 @@ function Location(city, data) {
 }
 
 // Weather Constructor
-function Weather(city, data){
+function Weather(data){
   this.forecast = data.weather.description;
   this.time = new Date(data.valid_date).toString().slice(0, 15);
   Weather.all.push(this);
